6aa8c43faa19dd8d09e12884fb743f325b2c782f
Smet <dimkazhr@gmail.com>
2020-05-13 19:34:00 +0300

    Create indexer for one thread

--- index/indexer.py
+++ index/indexer.py
@@ -5,15 +5,51 @@ from pathlib import Path
 class Indexer:
     def __init__(self):
         self.index_dict = {}
+        self.count = 0
 
-    def _parse_file(self, path: Path) -> list:
-        pass
+    @staticmethod
+    def _parse_file(path: Path) -> list:
+        symbols = ['.', ',', ';', '(', ')', '[', ']', ':', '?', '<' '>' '\\', '/']
+        text = path.read_text('utf-8')
 
-    def create_index(self, list_of_paths: list, num_of_threads: int = 1) -> dict:
-        pass
+        for symbol in symbols:
+            text = text.replace(symbol, '')
+
+        return text.split()
+
+    @staticmethod
+    def _generate_file_id(file_path: Path, dir_path_len: int) -> int:
+        match_dict = {
+            'test': '1',
+            'train': '2',
+            'neg': '1',
+            'pos': '2',
+            'unsup': '3',
+        }
+
+        d1, d2, file_name = str(file_path)[dir_path_len + 1:].split('\\')
+        file_id = match_dict[d1] + match_dict[d2] + file_name.split('_')[0]
+
+        return int(file_id)
+
+    def create_index(self, dir_path: Path, list_of_paths: list, num_of_threads: int = 1) -> dict:
+        dir_path_len = len(str(dir_path))
+        d = self._create_index_dict(list_of_paths, dir_path_len, {})
+        return d
 
     def _merge(self):
         pass
 
-    def _create_index_dict(self):
-        pass
+    def _create_index_dict(self, list_of_paths: list, dir_path_len: int, c_dict: dict):
+        for path in list_of_paths:
+            file_id = self._generate_file_id(path, dir_path_len)
+            lexemes = self._parse_file(path)
+
+            for lexeme in lexemes:
+                self.count += 1
+                try:
+                    c_dict[lexeme].add(file_id)
+                except KeyError:
+                    c_dict[lexeme] = {file_id, }
+
+        return c_dict
--- index/main.py
+++ index/main.py
@@ -2,17 +2,39 @@ from pathlib import Path
 import time
 
 from utils import generate_path_pattern, generate_list_of_paths
+from indexer import Indexer
 
 
 def main():
-    directory = 'C:/Users/Smet/Desktop/3Course/Term2/Parallel/aclImdb/aclImdb'
-    path = Path(directory)
+    dir_path = 'C:/Users/Smet/Desktop/3Course/Term2/Parallel/aclImdb/aclImdb'
+    path = Path(dir_path)
 
     pattern = generate_path_pattern()
     paths = generate_list_of_paths(path, pattern)
 
-    print(paths)
-    print(len(paths))
+    # print(paths)
+    # print(len(paths))
+
+    start = time.time()
+
+    indexer = Indexer()
+
+    # paths = [p for p in path.rglob('*.txt')]
+
+    d = indexer.create_index(path, paths)
+
+    end = time.time() - start
+
+    print(len(d))
+
+    print(f'{end}')
+    print(f'{indexer.count}')
+
+    c = 0
+    for k, i in d.items():
+        c += len(i)
+
+    print(c)
 
 
 if __name__ == '__main__':
